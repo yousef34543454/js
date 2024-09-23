a7504acf9e0c6dda8fcdb4602060d8413a7162a8
yousef34543454 <tommy.shelbi.47.o@gmail.com>
2024-09-23 07:48:11 -0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,24 @@
+console.log("salam");
+alert("khosh omadi")
+let first="ali"
+let last="ramezani"
+let number= 0
+console.log(first,typeof first);
+console.log(last,typeof last);
+console.log(number,typeof number);
+let age=18
+let x=100
+let y=50
+let person=prompt("please inter your age")
+if (age>18){
+
+let both = x * y
+document.querySelector("div#root").innerHTML=`<h2>ضرب:${both}</h2>`
+let ser = x + y
+document.querySelector("div#root").innerHTML+=`<h2>جمع:${ser}</h2>`
+let ter = x / y
+document.querySelector("div#root").innerHTML+=`<h2>جمع:${ter}</h2>`}
+else{
+
+    document.querySelector("div#root").innerHTML+=`<h2>اینجا مناسب سن تو نیست برو بیرون</h2>`}
+
